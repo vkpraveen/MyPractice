e95cf981fe09f50ca9dd548c4304d120aee8fde7
Praveen Kumar <Praveen.Vangapally@cdk.com>
2016-09-22 17:57:07 +0530

    Added Singleton.csproj

---
+++ C#/Design Patterns/Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CheckStandardSingleton();
+            CheckStandardSingletonFailureWithThreads();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Check for regular object creation based on single instace
+        /// </summary>
+        static void CheckStandardSingleton()
+        {
+            StandardSingleton standardSingletonObject = StandardSingleton.GetStandardSingletonInstance();
+            Console.WriteLine(standardSingletonObject.Add(1, 2));
+        }
+
+        /// <summary>
+        /// Check for object creation based multiple threads
+        /// </summary>
+        static void CheckStandardSingletonFailureWithThreads()
+        {
+            List<int> listofNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            try
+            {
+                Parallel.ForEach(listofNumbers, (number) =>
+                {
+                    StandardSingleton standardSingletonObject = StandardSingleton.GetStandardSingletonInstance();
+                    Console.WriteLine(standardSingletonObject.Add(1, 2));
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ C#/Design Patterns/Singleton/Singleton/Singleton/StandardSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class StandardSingleton
+    {
+        private static StandardSingleton _standardSingletonInstance;
+
+        private StandardSingleton() { }
+
+        public static StandardSingleton GetStandardSingletonInstance() => _standardSingletonInstance ?? (_standardSingletonInstance = new StandardSingleton());
+
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
